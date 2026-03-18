17d5ed27686a78e80400345013d4d9783f19e057
Your Name <you@example.com>
2026-03-18 18:30:33 +0100

    fix railway

--- maximoblog/server.js
+++ maximoblog/server.js
@@ -4,14 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-// Path to Angular build (after running `ng build --prod`)
-const DIST_FOLDER = path.join(__dirname, '../frontend/dist/MaximoBlogFront');
+// Serve the pre-built Angular files
+const DIST_FOLDER = path.join(__dirname, '../MaximoBlogFront/dist/MaximoBlogFront');
 app.use(express.static(DIST_FOLDER));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(DIST_FOLDER, 'index.html'));
+  res.sendFile(path.join(DIST_FOLDER, 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
